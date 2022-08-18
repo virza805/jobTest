d2def0aa96a4b4b39406fbb95962b7b02766043c
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2022-08-19 05:10:52 +0600

    nuxt.js setup

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,16 @@
+<template >
+  <div>
+    <footer class="main-footer">
+      <strong><a href="https://vir-za.com">Frontend Footer</a>.</strong> All rights reserved.
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
